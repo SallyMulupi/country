1b629c7b30497ccde8aa4302cc794b3562eea302
SallyMulupi <mulupisally2gmail.com>
2022-06-25 14:25:50 +0300

    add event listener click

--- src/index.js
+++ src/index.js
@@ -49,3 +49,14 @@ searchBtn.addEventListener("click", () => {
             }
         });
 });
+let countLikes = document.getElementById('like-count')
+
+let likeButton = document.getElementById('like-button')
+
+
+let integer = 0;
+
+likeButton.addEventListener('click', () => {
+    integer += 1
+    countLikes.innerHTML = `${integer} likes`;
+})
