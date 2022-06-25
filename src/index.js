ce64233abeb635f1364c2dbdb1d3bc7ad45c00cc
SallyMulupi <mulupisally2gmail.com>
2022-06-25 14:19:08 +0300

    change capital

--- src/index.js
+++ src/index.js
@@ -11,13 +11,12 @@ searchBtn.addEventListener("click", () => {
             //   console.log(data[0]);
             //   console.log(data[0].capital[0]);
             result.innerHTML = `
-                        <img src = "${data[0].flags.svg}"
-                        class = "flag-img" >
-                            < h2 > $ { data[0].name.common } < /h2> 
-                            <div class = "wrapper" >
-                            <div class = "data-wrapper" >
-                            <h4 > Capital: < /h4> <
-                            span > $ { data[0].capital[0] } < /span>
+                        <img src ="${data[0].flags.svg}"
+                        class = "flag-img">
+                            <h2>${data[0].name.common} </h2> 
+                            <div class = "wrapper">
+                            <div class = "data-wrapper">
+                            <h4> Capital: </h4><span>${ data[0].capital[0] }</span>
                              </div> 
                              </div><div class="wrapper">
                              <div class="data-wrapper">
