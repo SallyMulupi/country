70dfda2bd130a23198ea976e6fbc3981771d65d7
SallyMulupi <mulupisally2gmail.com>
2022-06-25 13:58:50 +0300

    add result inner html

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ searchBtn.addEventListener("click", () => {
                 .then((data) => {
                         //   console.log(data[0]);
                         //   console.log(data[0].capital[0]);
+                        result.innerHTML = `
+                        <img src = "${data[0].flags.svg}"
+                        class = "flag-img" >
+                            < h2 > $ { data[0].name.common } < /h2> 
+                            <div class = "wrapper" >
+                            <div class = "data-wrapper" >
+                            <h4 > Capital: < /h4> <
+                            span > $ { data[0].capital[0] } < /span>
+                             </div> 
+                             </div>
