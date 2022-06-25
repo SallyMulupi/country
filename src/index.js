f0139e136583ebfea586ae3b2a24590e6141813e
SallyMulupi <mulupisally2gmail.com>
2022-06-25 13:43:42 +0300

    fetch url comment

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ searchBtn.addEventListener("click", () => {
             let countryName = countryInp.value;
             let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
             console.log(finalURL);
+            //fecth url
